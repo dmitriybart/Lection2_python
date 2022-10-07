0a7b0aeedea702fa802829111c4c21354df15e14
Dmitriy B <kupperfeet@gmail.com>
2022-10-07 17:01:27 +0300

    first commit2

--- task_1.py
+++ task_1.py
@@ -1,5 +1,9 @@
-colors = ['red', 'green', ' blue']
-data = open ('file2a.txt', 'a') # а - доплоняет текст(не перезаписывает)
-data.writelines(colors)
-data.close()
-exit()#остфновка кода
+# colors = ['red', 'green', ' blue']
+# data = open('C:/Users/Dmitriy/Desktop/Разработчик ГБ/8Знакомство с языком Python/Lections2.0/Lesson_2/file2a.txt', 'a') # а - доплоняет текст(не перезаписывает)
+# data.writelines(colors)
+# data.close()
+# exit()#остановка кода
+
+with open ('file2a.txt', 'w') as data: # w - переписывает данные
+    data.write('line1\n')
+    data.write('line2\n')
